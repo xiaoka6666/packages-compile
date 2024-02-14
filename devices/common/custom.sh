5c0002dc9d8626d3162176ee9cf6d91e42f89768
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-02-14 18:08:34 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -8,52 +8,16 @@ do
 	[[ "$(grep "KernelPackage" "$ipk/Makefile")" && ! "$(grep "BuildPackage" "$ipk/Makefile")" ]] && rm -rf $ipk || true
 done
 
-<<'COMMENT'
-rm -Rf feeds/luci/{applications,collections,protocols,themes,libs,docs,contrib}
-rm -Rf feeds/luci/modules/!(luci-base)
-rm -Rf feeds/packages/!(lang|libs|devel|utils|net|multimedia)
-rm -Rf feeds/packages/multimedia/!(gstreamer1|ffmpeg)
-rm -Rf feeds/packages/libs/libcups
-rm -Rf feeds/packages/net/!(mosquitto|curl)
-rm -Rf feeds/base/package/firmware
-rm -Rf feeds/base/package/network/!(services|utils)
-rm -Rf feeds/base/package/network/services/!(ppp)
-rm -Rf feeds/base/package/system/!(opkg|ubus|uci|ca-certificates)
-rm -Rf feeds/base/package/kernel/!(cryptodev-linux)
-COMMENT
 
-status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kiddin9/openwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
-while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
-echo "wait 5s"
-sleep 5
-status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kiddin9/openwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
-done
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a -p kiddin9 -f
 ./scripts/feeds install -a
 
-sed -i 's/\(page\|e\)\?.acl_depends.*\?}//' `find package/feeds/kiddin9/luci-*/luasrc/controller/* -name "*.lua"`
-# sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/kiddin9/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
-sed -i 's/Os/O2/g' include/target.mk
-
-sed -i \
-	-e "s/+\(luci\|luci-ssl\|uhttpd\)\( \|$\)/\2/" \
-	-e "s/+nginx\( \|$\)/+nginx-ssl\1/" \
-	-e 's/+python\( \|$\)/+python3/' \
-	-e 's?../../lang?$(TOPDIR)/feeds/packages/lang?' \
-	-e 's,$(STAGING_DIR_HOST)/bin/upx,upx,' \
-	package/feeds/kiddin9/*/Makefile
 
 cp -f devices/common/.config .config
-mv feeds/base feeds/base.bak
-mv feeds/packages feeds/packages.bak
+
 make defconfig
-rm -Rf tmp
-mv feeds/base.bak feeds/base
-mv feeds/packages.bak feeds/packages
-sed -i 's/CONFIG_ALL=y/CONFIG_ALL=n/' .config
-sed -i '/PACKAGE_kmod-/d' .config
 
 sed -i "/mediaurlbase/d" package/feeds/*/luci-theme*/root/etc/uci-defaults/*
 
